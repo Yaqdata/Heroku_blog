4093ef859c9d58e80ccd9e4c7da720e73deebadb
Yaq <yaqdata@gmail.com>
2013-12-20 17:19:58 +0800

    html

--- website/mobile/urls.py
+++ website/mobile/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import patterns, include, url
 from mobile.views import *
 
 urlpatterns = patterns('mobile.views.blog',
-    url(r'^(?P<slug>[\w\-]+)/$', 'post'),
     url(r'^$', 'index'),
+    url(r'^(?P<slug>[\w\-]+)/$', 'post'),
 )
