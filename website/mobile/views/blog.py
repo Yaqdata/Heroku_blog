a3845dc37da698df2087abaef8a1267c0dec684c
Yaq <yaqdata@gmail.com>
2013-12-23 18:25:56 +0800

    add sina blog

--- website/mobile/views/blog.py
+++ website/mobile/views/blog.py
@@ -2,10 +2,15 @@
 from django.shortcuts import render, get_object_or_404
 from module.blog.models import Post
 
+def base(request):
+    top_clicks = 'xxx'
+
 def index(request):
     posts = Post.objects.filter(published=True)
     return render(request, 'blog/index.html', {'posts': posts})
 
 def post(request, slug):
+    print slug
     post = get_object_or_404(Post, slug=slug)
+    print post.pk
     return render(request, 'blog/post.html', {'post': post})
